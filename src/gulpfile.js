648c143aa0af2ff0e8f32a9b3324aa09f0bd3ebf
vexleet <abda70226@gmail.com>
2021-11-17 20:58:18 +0100

    migrate gulp 3 to 4

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -7,8 +7,8 @@ const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 
-gulp.task('sass', function () {
-  return gulp
+gulp.task('sass', function (cb) {
+  gulp
     .src('app/scss/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
@@ -16,15 +16,12 @@ gulp.task('sass', function () {
     .pipe(cssnano())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('app/css'))
-    .pipe(
-      browserSync.reload({
-        stream: true,
-      })
-    );
+    .pipe(browserSync.stream());
+  cb();
 });
 
-gulp.task('js', function () {
-  return gulp
+gulp.task('js', function (cb) {
+  gulp
     .src('app/js/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(
@@ -35,19 +32,24 @@ gulp.task('js', function () {
     .pipe(concat('all.js'))
     .pipe(uglify())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('app/scripts'));
+    .pipe(gulp.dest('app/scripts'))
+    .pipe(browserSync.stream());
+  cb();
 });
 
-gulp.task('browserSync', function () {
+gulp.task('browserSync', function (done) {
   browserSync.init({
     server: {
       baseDir: 'app',
     },
   });
+  done();
 });
 
-gulp.task('watch', ['browserSync', 'sass', 'js'], function () {
-  gulp.watch('app/scss/**/*.scss', ['sass']);
-  gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/js/**/*.js', ['js'], browserSync.reload);
+gulp.task('watch-files', function () {
+  gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('app/*.html').on('change', browserSync.reload);
+  gulp.watch('app/js/**/*.js', gulp.series('js'));
 });
+
+gulp.task('watch', gulp.series('browserSync', 'sass', 'js', 'watch-files'));
