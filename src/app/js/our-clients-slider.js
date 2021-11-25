4c67a4001df25fe6f8f9f6b9559c4521802c832b
vexleet <abda70226@gmail.com>
2021-11-25 22:28:51 +0100

    add our clients

---
+++ src/app/js/our-clients-slider.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars,no-undef
+const slider = tns({
+  container: '.reviews-wrapper',
+  items: 1,
+  controls: false,
+  navPosition: 'bottom',
+  fixedWidth: 290,
+  center: true,
+  responsive: {
+    1024: {
+      disable: true,
+    },
+  },
+});
