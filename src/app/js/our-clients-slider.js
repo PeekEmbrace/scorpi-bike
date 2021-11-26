32763e1a5bc1a9189d67eab666faa187e8586a72
vexleet <abda70226@gmail.com>
2021-11-26 17:11:39 +0100

    changed slider library and did some fixes

--- src/app/js/our-clients-slider.js
+++ src/app/js/our-clients-slider.js
@@ -1,14 +1,15 @@
-// eslint-disable-next-line no-unused-vars,no-undef
-const slider = tns({
-  container: '.reviews-wrapper',
-  items: 1,
-  controls: false,
-  navPosition: 'bottom',
-  fixedWidth: 290,
-  center: true,
-  responsive: {
-    1024: {
-      disable: true,
-    },
+// eslint-disable-next-line no-undef,no-unused-vars
+const swiper = new Swiper('.swiper  ', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.reviews-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
   },
 });
