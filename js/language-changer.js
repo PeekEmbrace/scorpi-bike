2b168a61bfc4c6fb02bdbec6c3bbc5c70e466175
vexleet <abda70226@gmail.com>
2022-01-20 22:45:09 +0100

    removed unnecessary js and fix pages

--- js/language-changer.js
+++ js/language-changer.js
@@ -1,21 +1,8 @@
 (() => {
   const languageChangeWrapper = document.querySelector('.language-change');
-  const languages = document.querySelectorAll('.languages li');
-  const currentLanguage = document.getElementById('lang');
-
-  const pageLanguage = window.location.pathname;
 
   languageChangeWrapper.addEventListener('click', () => {
     languageChangeWrapper.classList.toggle('active');
   });
-
-  languages.forEach((language) => {
-    const elementLang = language.getAttribute('data-lang');
-    if(pageLanguage.includes(elementLang)) {
-      language.classList.add('active');
-      currentLanguage.innerText = elementLang.toUpperCase();
-    }
-  });
-
 })();
 
