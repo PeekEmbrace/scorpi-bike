eeba118c29ae9aaffad0c8b81a3791cbc0e39f8c
Plamen Michev <plamenmichev15@gmail.com>
2023-12-18 16:31:18 +0100

    cleanup

--- js/copyright-year.js
+++
@@ -1,4 +0,0 @@
-const copyrightYear = document.getElementById('copyright-year');
-const currentYear = new Date().getFullYear();
-
-copyrightYear.innerText = currentYear > 2021 ? `-${currentYear}` : '';
--- js/language-changer.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const languageChangeWrapper = document.querySelector('.language-change');
-
-  languageChangeWrapper.addEventListener('click', () => {
-    languageChangeWrapper.classList.toggle('active');
-  });
-})();
-
